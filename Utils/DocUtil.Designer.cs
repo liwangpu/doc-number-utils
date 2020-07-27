f67308f14f1302c4c87d51b5aa1079fc8dfbfb32
leon <puliwang@cxist.com>
2020-07-27 23:17:45 +0800

    基本完成档号生成

--- Utils/DocUtil.Designer.cs
+++ Utils/DocUtil.Designer.cs
@@ -32,6 +32,10 @@
             this.txtSourcePath = new System.Windows.Forms.TextBox();
             this.btnSourceUpload = new System.Windows.Forms.Button();
             this.btnTransfer = new System.Windows.Forms.Button();
+            this.lbMsg = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDocFolder = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -39,21 +43,21 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.Size = new System.Drawing.Size(59, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "数据源:";
+            this.label1.Text = "表格目录:";
             // 
             // txtSourcePath
             // 
-            this.txtSourcePath.Location = new System.Drawing.Point(65, 16);
+            this.txtSourcePath.Location = new System.Drawing.Point(75, 16);
             this.txtSourcePath.Name = "txtSourcePath";
             this.txtSourcePath.ReadOnly = true;
-            this.txtSourcePath.Size = new System.Drawing.Size(184, 21);
+            this.txtSourcePath.Size = new System.Drawing.Size(214, 21);
             this.txtSourcePath.TabIndex = 1;
             // 
             // btnSourceUpload
             // 
-            this.btnSourceUpload.Location = new System.Drawing.Point(255, 14);
+            this.btnSourceUpload.Location = new System.Drawing.Point(295, 14);
             this.btnSourceUpload.Name = "btnSourceUpload";
             this.btnSourceUpload.Size = new System.Drawing.Size(75, 23);
             this.btnSourceUpload.TabIndex = 2;
@@ -63,7 +67,8 @@
             // 
             // btnTransfer
             // 
-            this.btnTransfer.Location = new System.Drawing.Point(255, 43);
+            this.btnTransfer.Enabled = false;
+            this.btnTransfer.Location = new System.Drawing.Point(295, 44);
             this.btnTransfer.Name = "btnTransfer";
             this.btnTransfer.Size = new System.Drawing.Size(75, 23);
             this.btnTransfer.TabIndex = 2;
@@ -71,18 +76,58 @@
             this.btnTransfer.UseVisualStyleBackColor = true;
             this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
             // 
-            // Util
+            // lbMsg
+            // 
+            this.lbMsg.AutoSize = true;
+            this.lbMsg.Location = new System.Drawing.Point(73, 84);
+            this.lbMsg.Name = "lbMsg";
+            this.lbMsg.Size = new System.Drawing.Size(65, 12);
+            this.lbMsg.TabIndex = 15;
+            this.lbMsg.Text = "待上传表格";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "操作消息:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "档号目录:";
+            // 
+            // txtDocFolder
+            // 
+            this.txtDocFolder.Location = new System.Drawing.Point(75, 44);
+            this.txtDocFolder.Name = "txtDocFolder";
+            this.txtDocFolder.Size = new System.Drawing.Size(214, 21);
+            this.txtDocFolder.TabIndex = 17;
+            // 
+            // DocUtil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 106);
+            this.ClientSize = new System.Drawing.Size(382, 105);
+            this.Controls.Add(this.txtDocFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbMsg);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnTransfer);
             this.Controls.Add(this.btnSourceUpload);
             this.Controls.Add(this.txtSourcePath);
             this.Controls.Add(this.label1);
-            this.Name = "Util";
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(398, 144);
+            this.Name = "DocUtil";
             this.ShowIcon = false;
-            this.Text = "档号小工具";
+            this.Text = "门类档案目录批量处理工具";
             this.Load += new System.EventHandler(this.Util_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,6 +140,10 @@
         private System.Windows.Forms.TextBox txtSourcePath;
         private System.Windows.Forms.Button btnSourceUpload;
         private System.Windows.Forms.Button btnTransfer;
+        private System.Windows.Forms.Label lbMsg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDocFolder;
     }
 }
 
--- Utils/DocUtil.cs
+++ Utils/DocUtil.cs
@@ -1,20 +1,17 @@
-﻿using System;
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using EpplusHelper;
-using OfficeOpenXml;
 
 namespace Utils
 {
     public partial class DocUtil : Form
     {
-        protected string tmpXlsxPath;
         public DocUtil()
         {
             InitializeComponent();
@@ -22,9 +19,7 @@ namespace Utils
 
         private void Util_Load(object sender, EventArgs e)
         {
-            txtSourcePath.Text = @"C:\Users\Leon\Desktop\工作簿1.xls";
-            tmpXlsxPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".xlsx");
-            XLSToXLSXConverter.Convert(txtSourcePath.Text, tmpXlsxPath);
+            //txtSourcePath.Text = @"C:\Users\Leon\Desktop\档号\zpda_DrMbGZ2(1).xls";
         }
 
         private void btnSourceUpload_Click(object sender, EventArgs e)
@@ -37,28 +32,84 @@ namespace Utils
             if (OpenFileDialog1.ShowDialog() == DialogResult.OK)//如果点的是确定就得到文件路径
             {
                 txtSourcePath.Text = OpenFileDialog1.FileName;
-                tmpXlsxPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".xlsx");
-                XLSToXLSXConverter.Convert(txtSourcePath.Text, tmpXlsxPath);
+                btnTransfer.Enabled = true;
+                ShowMsg("表格已经上传,请进行数据处理");
             }
         }
 
         private void btnTransfer_Click(object sender, EventArgs e)
         {
-            var source = new List<DocNumberMapping>();
+            var list = new List<DocNumberMapping>();
             var readData = new Action(() =>
             {
-                if (!string.IsNullOrEmpty(tmpXlsxPath))
+                if (!string.IsNullOrEmpty(txtSourcePath.Text))
                 {
-                    using (var package = new ExcelPackage(new FileInfo(tmpXlsxPath)))
+                    ShowMsg("数据处理中,请稍后");
+                    HSSFWorkbook hssfwb;
+                    using (FileStream file = new FileStream(txtSourcePath.Text, FileMode.Open, FileAccess.Read))
                     {
-                        var worksheet = package.Workbook.Worksheets[1];
-                        source = SheetReader<DocNumberMapping>.From(worksheet);
+                        hssfwb = new HSSFWorkbook(file);
+                    }
+                    ISheet sheet = hssfwb.GetSheetAt(0);
+                    sheet.GetRow(0).GetCell(15).SetCellValue("档号");
+                    for (int row = 1; row <= sheet.LastRowNum; row++)
+                    {
+                        var currentRow = sheet.GetRow(row);
+                        if (currentRow != null)
+                        {
+                            var doc = new DocNumberMapping();
+                            var c1 = sheet.GetRow(row).GetCell(1);
+                            if (c1 != null)
+                                doc._张号 = c1.NumericCellValue;
+                            var c4 = sheet.GetRow(row).GetCell(4);
+                            if (c4 != null)
+                                doc._全宗号 = c4.NumericCellValue;
+                            var c5 = sheet.GetRow(row).GetCell(5);
+                            if (c5 != null)
+                                doc._年度 = c5.NumericCellValue;
+                            var c7 = sheet.GetRow(row).GetCell(7);
+                            if (c7 != null)
+                                doc._业务类别 = c7.StringCellValue;
+                            var c15 = currentRow.CreateCell(15);
+                            c15.SetCellValue(doc._档号);
+                            list.Add(doc);
+                        }
+                    }
+                    sheet.AutoSizeColumn(15);
+
+                    using (FileStream file = new FileStream(txtSourcePath.Text, FileMode.Open, FileAccess.Write))
+                    {
+                        hssfwb.Write(file);
                     }
                 }
             });
 
             readData.BeginInvoke((obj) =>
             {
+                GenerateFolder(list.Select(x => x._档号).Distinct().ToList());
+            }, null);
+        }
+
+        private void GenerateFolder(List<string> folders)
+        {
+
+            InvokeMainForm((obj) =>
+            {
+                FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var dir = folderDialog.SelectedPath;
+                    txtDocFolder.Text = dir;
+                    folders.ForEach(doc =>
+                    {
+                        var p = Path.Combine(dir, doc);
+                        if (!Directory.Exists(p))
+                            Directory.CreateDirectory(p);
+                        ShowMsg("档号生成完毕");
+                    });
+
+
+                }
 
             }, null);
         }
@@ -88,12 +139,59 @@ namespace Utils
         }
         #endregion
 
+        #region ShowMsg 消息提示
+        /// <summary>
+        /// 消息提示
+        /// </summary>
+        /// <param name="strMsg"></param>
+        private void ShowMsg(string strMsg)
+        {
+            if (this.InvokeRequired)
+            {
+                var act = new Action<string>(ShowMsg);
+                this.Invoke(act, strMsg);
+            }
+            else
+            {
+                this.lbMsg.Text = strMsg;
+            }
+        }
+        #endregion
+
     }
 
     public class DocNumberMapping
     {
-        [ExcelColumn("保管期限")]
-        public string _保管期限 { get; set; }
-
+        public double _张号 { get; set; }
+        public double _全宗号 { get; set; }
+        public double _年度 { get; set; }
+        public string _业务类别 { get; set; }
+        public string _档号
+        {
+            get
+            {
+                StringBuilder bd = new StringBuilder();
+                if (_全宗号 > 0)
+                {
+                    bd.Append($"{_全宗号}-");
+                }
+                bd.Append("ZP-");
+                bd.Append($"{_年度}-");
+                switch (_业务类别)
+                {
+                    case "会务活动":
+                        bd.Append("hw-");
+                        break;
+                    case "基建项目":
+                        bd.Append("jj-");
+                        break;
+                    case "实物":
+                        bd.Append("sw-");
+                        break;
+                }
+                bd.Append(_张号.ToString().PadLeft(4, '0'));
+                return bd.ToString();
+            }
+        }
     }
 }
